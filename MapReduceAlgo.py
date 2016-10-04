e0b4d9c027c0a6ca4ec86920645b7bdaf56dab3e
Céline Lay <clay@edu.ece.fr>
2016-10-04 18:37:34 +0200

    Shuffle/sort and reduce done

--- MapReduceAlgo.py
+++ MapReduceAlgo.py
@@ -1,23 +1,63 @@
 def map(line):
-	line = line.replace(',', '')
-	#Replace all punctuation
 	words = line.split()
 	mapper = []
 	for word in words:
 		item = (word, 1)
 		mapper.append(item)
-	#To remove later
-	print(mapper)
+	return mapper
 
-#def shuffleAndSort():
+def shuffleAndSort(mappedWords):
+	#sort alphabetically
+	sortedList = sorted(mappedWords, key=lambda mappedWords: mappedWords[0].lower())
+	
+	factorizedList = []
+	occurences = 1
+	count=0
+	while count != len(sortedList)-1:
+		if sortedList[count][0]==sortedList[count+1][0]:
+			occurences = occurences + 1
+		else:
+			if occurences == 1:
+				factorizedList.append(sortedList[count])
+			else:
+				iteration = []
+				for j in range (0, occurences):
+					iteration.append(1)
+				tup = (sortedList[count][0], iteration)
+				factorizedList.append(tup)
+				occurences = 1
+		count = count + 1
 
-#def reduce():
+	return factorizedList
+
+def reduce(shuffled):
+	reduced = []
+	for tup in shuffled:
+		if tup[1] == 1:
+			reduced.append(tup)
+		else:
+			item = (tup[0],len(tup[1]))
+			reduced.append(item)
+	return reduced
 
 def main():
+	punctuations = ['@','#','&','"','(',')','§','!','[',']','{','}',',','?','.',';',':','/','<','>','+','=','-','_']
+	#Open the file to count words from
 	file = open("test.txt", "r")
+	#Split the text into lines and put them in a list (Input Reader)
 	lines = file.readlines()
+	#Close the file
 	file.close()
+
+	mapped = []
 	for line in lines:
-		map(line)
+		for character in line:
+			if character in punctuations:
+				line = line.replace(character, '')
+		mapped.extend(map(line))
+
+	shuffledTuples = shuffleAndSort(mapped)
+	output = reduce(shuffledTuples)
+	print(output)
 
 main()
